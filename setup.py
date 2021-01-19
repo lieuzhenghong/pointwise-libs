6503304b7e1083b4dab0eb563a8a9a4b6a525c97
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2021-01-20 00:40:22 +0800

    Get build working

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pointwise_libs",
-    version="0.0.1",
+    version="0.0.5",
     author="Lieu Zheng Hong",
     author_email="lieuzhenghong@gmail.com",
     description="Helper functions for my political science research",
